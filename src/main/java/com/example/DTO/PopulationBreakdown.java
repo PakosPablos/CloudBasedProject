c2c3f4182f586814b2dc9d48dce2b611db1310ee
PakosPablos <danielmatospedroso@outlook.com>
2025-11-24 16:44:46 +0100

    initialization

---
+++ src/main/java/com/example/DTO/PopulationBreakdown.java
@@ -0,0 +1,30 @@
+package com.example.DTO;
+
+public class PopulationBreakdown {
+    private long total;
+    private long males;
+    private long females;
+    
+    
+     
+    public long getTotal() {
+        return total;
+    }
+    public void setTotal(long total) {
+        this.total = total;
+    }
+    public long getMales() {
+        return males;
+    }
+    public void setMales(long males) {
+        this.males = males;
+    }
+    public long getFemales() {
+        return females;
+    }
+    public void setFemales(long females) {
+        this.females = females;
+    }
+
+    
+}
---
+++ src/main/java/com/example/demo/PopulationController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import com.example.DTO.PopulationBreakdown;
+import com.example.DTO.YearPopulation;
+
+public class PopulationController {
+
+    private YearPopulation yearPopulation;
+
+    public PopulationController(YearPopulation yearPopulation) {
+        this.yearPopulation = yearPopulation;
+    }
+
+    
+    
+}
